3be9b8e08650d51d472885527218852b2da6848a
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2024-09-11 08:15:42 -0500

    Added a function for generating latex code from a boolean function from the program

--- convertFunc.py
+++ convertFunc.py
@@ -1,9 +1,11 @@
 def toWAL(expr):
-    expr = expr.replace('+', '||')
-    expr = expr.replace('*', '&&')
+    expr = expr.replace('+', '||').replace('*', '&&')
     return expr
 
 def toPY(expr):
-    expr = expr.replace('||', '+')
-    expr = expr.replace('&&', '*')
+    expr = expr.replace('||', '+').replace('&&', '*')
+    return expr
+
+def PYtoLatexCode(expr):
+    expr = expr.replace('+', '\lor').replace('*', '\land').replace('!', '\lnot')
     return expr
